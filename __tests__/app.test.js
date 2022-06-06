5493ee83def7696a23eed7efe801e0872a63e665
Julie Nisbet <jmnisbet@gmail.com>
2022-06-06 11:29:56 -0700

    adding dog detail

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { get } = require('../lib/app');
 const { dogs } = require('../data/dogs');
 
 describe('dogs-api routes', () => {
@@ -10,9 +9,17 @@ describe('dogs-api routes', () => {
     return setup(pool);
   });
 
-  it('should return a list of dogs', async () => {
+  it('/dogs should return a list of dogs', async () => {
     const res = await request(app).get('/dogs');
     expect(res.body).toEqual(dogs);
+  });
+
+  it('/dogs/:id should return dog details', async () => {
+    const res = await request(app).get('/dogs/1');
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'Benny',
+    });
   });
 
   afterAll(() => {
--- data/dogs.js
+++ data/dogs.js
@@ -1,8 +1,8 @@
 const dogs = [
-  { id: 1, name: 'Benny' },
-  { id: 2, name: 'Finley' },
-  { id: 3, name: 'Ruby' },
-  { id: 4, name: 'Jeep' },
+  { id: '1', name: 'Benny' },
+  { id: '2', name: 'Finley' },
+  { id: '3', name: 'Ruby' },
+  { id: '4', name: 'Jeep' },
 ];
 
 module.exports = { dogs };
