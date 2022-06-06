c79bf80c6c8e284623000907454a8fdf91dde19f
Julie Nisbet <jmnisbet@gmail.com>
2022-06-06 11:22:51 -0700

    adding dog list

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,17 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { get } = require('../lib/app');
+const { dogs } = require('../data/dogs');
 
 describe('dogs-api routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('should return a list of dogs', async () => {
+    const res = await request(app).get('/dogs');
+    expect(res.body).toEqual(dogs);
   });
 
   afterAll(() => {
